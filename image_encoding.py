c816d6f6396d85016228a9520735b4a2ac8b425f
Qcymaq <quancsf@gmail.com>
2024-07-17 14:36:15 +0700

    Use Clip from huggingFace

--- image_encoding.py
+++ image_encoding.py
@@ -2,10 +2,10 @@ import os
 import glob
 from pathlib import Path
 import torch
-import clip
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import numpy as np
+from transformers import CLIPProcessor, CLIPModel
 
 class ImageDataset(Dataset):
     def __init__(self, image_paths, preprocess):
@@ -18,7 +18,7 @@ class ImageDataset(Dataset):
     def __getitem__(self, idx):
         image_path = self.image_paths[idx]
         image = Image.open(image_path).convert("RGB")
-        image = self.preprocess(image)
+        image = self.preprocess(images=image, return_tensors="pt")["pixel_values"].squeeze(0)
         return image
 
 def get_data_paths(dir: str | list[str], data_formats: list, prefix: str = '') -> list[str]:
@@ -35,14 +35,15 @@ def get_data_paths(dir: str | list[str], data_formats: list, prefix: str = '') -
     except Exception as e:
         raise Exception(f'{prefix}Error loading data from {dir}: {e}') from e
 
-def get_image_embeddings(data_dir, model_name="ViT-B/32", batch_size=32, device="cuda" if torch.cuda.is_available() else "cpu"):
-    # Load the CLIP model
-    model, preprocess = clip.load(model_name, device=device)
+def get_image_embeddings(data_dir, model_name="openai/clip-vit-base-patch32", batch_size=32, device="cuda" if torch.cuda.is_available() else "cpu"):
+    # Load the CLIP model from Hugging Face
+    model = CLIPModel.from_pretrained(model_name).to(device)
+    processor = CLIPProcessor.from_pretrained(model_name)
     
     # Create a dataset and dataloader
     image_paths = get_data_paths(data_dir, data_formats=["jpg", "jpeg", "png"])
     print(f"Found {len(image_paths)} images.")
-    dataset = ImageDataset(image_paths, preprocess)
+    dataset = ImageDataset(image_paths, processor)
     dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=4)
     
     # List to store image embeddings
@@ -52,7 +53,7 @@ def get_image_embeddings(data_dir, model_name="ViT-B/32", batch_size=32, device=
     with torch.no_grad():
         for images in dataloader:
             images = images.to(device)
-            embeddings = model.encode_image(images)
+            embeddings = model.get_image_features(pixel_values=images)
             embeddings /= embeddings.norm(dim=-1, keepdim=True)
             image_embeddings.append(embeddings.cpu().numpy())
 
@@ -61,5 +62,6 @@ def get_image_embeddings(data_dir, model_name="ViT-B/32", batch_size=32, device=
     
     return image_embeddings, image_paths
 
+# Use example
 data_dir = "data/coco-128/train"
 image_embeddings, image_paths = get_image_embeddings(data_dir)
