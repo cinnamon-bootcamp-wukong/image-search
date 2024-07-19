d0ffc8d55e0e54602a8b5b13e451248cc9db4013
Đỗ Nguyễn Duy Hoàng <60732952+hoang1645@users.noreply.github.com>
2024-07-19 23:03:43 +0700

    change the endpoint's ip (to broadcast) and port (avoid conflict w/ encode endpoint)

--- search_endpoint.py
+++ search_endpoint.py
@@ -57,4 +57,4 @@ async def similarity_search(file: UploadFile, request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8500)
