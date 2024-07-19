6737db24c4b1dd5072ea4b299e73010f717b2e51
Đỗ Nguyễn Duy Hoàng <60732952+hoang1645@users.noreply.github.com>
2024-07-19 23:05:15 +0700

    update encode endpoint ip

--- encode.py
+++ encode.py
@@ -88,4 +88,4 @@ async def embedding_function(file : UploadFile):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
